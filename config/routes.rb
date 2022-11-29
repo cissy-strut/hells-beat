de24fac86442ae0ed9ce101b6dd5bbd95caa60a1
Le Wagon <johann.lemarie@gmail.com>
2022-11-29 12:44:46 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :chatrooms, only: :show do
+  resources :matchings, only: %i[index show] do
+    member do
+      patch 'accept'
+      patch 'refuse'
+    end
+  end
+
+  resources :chatrooms, only: %i[show create index] do
     resources :messages, only: :create
   end
 end
