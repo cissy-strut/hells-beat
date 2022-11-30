beca5ccfda8ba63bcbaffa48165ad9927ccd080d
Le Wagon <johann.lemarie@gmail.com>
2022-11-30 20:45:33 +0100

    Matching model change

--- app/models/matching.rb
+++ app/models/matching.rb
@@ -3,4 +3,12 @@ class Matching < ApplicationRecord
 
   validates :user_one_status, presence: true
   validates :user_two_status, presence: true
+
+  def second_associated_user
+    User.find(user_two_id)
+  end
+
+  def first_associated_user
+    Self.user
+  end
 end
