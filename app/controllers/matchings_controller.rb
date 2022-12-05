73d13857b321cf039c6d5b69d63175566b057b84
Le Wagon <johann.lemarie@gmail.com>
2022-12-05 11:55:31 +0100

    acceptrefuse completed

--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -1,12 +1,18 @@
 class MatchingsController < ApplicationController
   def index
-    @matchings = Matching.all
-  end
-
-  def refuse
+    @matchings = current_user.matchings.where(user_one_status: "pending") + Matching.where(user_two_id: current_user.id).where(user_two_status: "pending")
   end
 
   def accept
+    @matching = Matching.find(params[:id])
+    @matching.update(user_one_status: 'accept')
+    redirect_to matchings_path
+  end
+
+  def refuse
+    @matching = Matching.find(params[:id])
+    @matching.update(user_one_status: 'refuse')
+    redirect_to matchings_path
   end
 
   def show
