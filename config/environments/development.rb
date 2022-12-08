30bb51411b86e848301c3d7517721d5e3e882696
Le Wagon <johann.lemarie@gmail.com>
2022-12-08 14:19:14 +0100

    PWA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "itchy-trains-reply-92-128-95-49.loca.lt"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
