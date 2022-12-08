fa42270bcca9db37d0e2df844d7b599f57cf63d5
Sylvain PEREZ MORILLAS <spfilm33@gmail.com>
2022-12-08 17:10:08 +0100

    dedefefe

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   EXPERIENCES = ['Débutant', 'Confirmé', 'Expert']
   STYLES = ["Rock'n'roll", "Blues", "Jazz", "Funk", "Rap", "Electro", "Pop", "Reggea", "Métal"]
 
+  # after_create :create_raw_matchings
+
   has_many :matchings
 
   has_one_attached :photo
@@ -23,4 +25,10 @@ class User < ApplicationRecord
   def age
     (Date.today.strftime('%Y%m%d').to_i - birth_date.strftime('%Y%m%d').to_i) / 10_000
   end
+
+  # def create_raw_matchings
+  #   (User.where.not(id: self.id)).each do |user|
+  #     Matching.create(user: self, user_two_id: user.id, user_one_status: "pending", user_two_status: "pending")
+  #   end
+  # end
 end
